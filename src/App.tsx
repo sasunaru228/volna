7736c697191ee42336fe3a426fe7575facd09dca
dmitrijorlov <od26102000@gmail.com>
2024-12-26 18:51:44 +0300

    ios support for fullscreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import {StoreProvider} from "./stores/StoreContext.tsx"
 import MainComponent from "./components/MainComponent/MainComponent.tsx"
 
+
+window.addEventListener('load', function() {
+    // Прокрутка на 1 пиксель вниз для скрытия строки поиска
+    window.scrollTo(0, 1);
+});
+
 function App() {
 
     return (
